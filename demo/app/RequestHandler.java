79d97e4e74f7557d88d5f2d1a7107380303ad773
acslk <d_vd415@hotmail.com>
2016-11-22 03:00:08 -0500

    update

--- demo/app/RequestHandler.java
+++ demo/app/RequestHandler.java
@@ -3,12 +3,30 @@ import io.netty.handler.codec.http.HttpContent;
 import io.netty.handler.codec.http.HttpRequest;
 import u.http.Responses;
 import uExt.routing.Routing;
+import uExt.template.HandlebarsTemplateEngine;
+
+import java.io.IOException;
 
 public class RequestHandler {
 
     public FullHttpResponse handle(HttpRequest request, HttpContent content) {
 
-        return Routing.routing(request, content);
+        System.out.println("entered");
+
+        try {
+            HandlebarsTemplateEngine a = new HandlebarsTemplateEngine();
+            String ret = "failed";
+            try {
+                ret = a.render("a.html", "a");
+            } catch (IOException e) {
+                System.out.println("failed");
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        //return Routing.routing(request, content);
+        return Responses.ok("hello");
     }
 
 }
